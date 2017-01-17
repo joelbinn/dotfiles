d6c23f6662cfa11cccdf798eeb52f70f216af592
Joel Binnquist <joel.binnquist@tillvaxtverket.se>
2017-01-17 10:23:39 +0100

    tnt

--- zsh/custom/my-patches.zsh
+++ zsh/custom/my-patches.zsh
@@ -11,7 +11,7 @@ export LC_ALL=$LANG
 export NLS_LANG=SWEDISH_SWEDEN.UTF8
 #PATH=$PATH:$ORACLE_HOME
 
-if [ "$JOEBIN_SH_PATH_SETUP" = "" ]; then
+if [ "$JOEBIN_SH_PATH_SETUP" = ""  ]; then
   export PROJ_DISK=/Volumes/projects-40g
   export PROJSTUFF=$PROJ_DISK/projectstuff
   #export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
@@ -132,8 +132,6 @@ pwcrt-start() {
     node server.js &;
     popd;
 }
-
-
 
 # NYA DOCKER DB KOMMANDON
 db-dump-to-docker-oraexp() {
@@ -262,6 +260,43 @@ dockerExecBash() {
     fi
 
     docker exec -it $1 bash
+}
+
+# Wrapper function for Maven's mvn command.
+# mvn-color based on https://gist.github.com/1027800
+export BOLD=`tput bold`
+export UNDERLINE_ON=`tput smul`
+export UNDERLINE_OFF=`tput rmul`
+export TEXT_BLACK=`tput setaf 0`
+export TEXT_RED=`tput setaf 1`
+export TEXT_GREEN=`tput setaf 2`
+export TEXT_YELLOW=`tput setaf 3`
+export TEXT_BLUE=`tput setaf 4`
+export TEXT_MAGENTA=`tput setaf 5`
+export TEXT_CYAN=`tput setaf 6`
+export TEXT_WHITE=`tput setaf 7`
+export BACKGROUND_BLACK=`tput setab 0`
+export BACKGROUND_RED=`tput setab 1`
+export BACKGROUND_GREEN=`tput setab 2`
+export BACKGROUND_YELLOW=`tput setab 3`
+export BACKGROUND_BLUE=`tput setab 4`
+export BACKGROUND_MAGENTA=`tput setab 5`
+export BACKGROUND_CYAN=`tput setab 6`
+export BACKGROUND_WHITE=`tput setab 7`
+export RESET_FORMATTING=`tput sgr0`
+mvn-color() {
+  (
+  # Filter mvn output using sed. Before filtering set the locale to C, so invalid characters won't break some sed implementations
+  unset LANG
+  LC_CTYPE=C mvn $@ | sed -e "s/\(\[INFO\]\)\(.*\)/${TEXT_BLUE}${BOLD}\1${RESET_FORMATTING}\2/g" \
+               -e "s/\(\[INFO\]\ BUILD SUCCESSFUL\)/${BOLD}${TEXT_GREEN}\1${RESET_FORMATTING}/g" \
+               -e "s/\(\[WARNING\]\)\(.*\)/${BOLD}${TEXT_YELLOW}\1${RESET_FORMATTING}\2/g" \
+               -e "s/\(\[ERROR\]\)\(.*\)/${BOLD}${TEXT_RED}\1${RESET_FORMATTING}\2/g" \
+               -e "s/Tests run: \([^,]*\), Failures: \([^,]*\), Errors: \([^,]*\), Skipped: \([^,]*\)/${BOLD}${TEXT_GREEN}Tests run: \1${RESET_FORMATTING}, Failures: ${BOLD}${TEXT_RED}\2${RESET_FORMATTING}, Errors: ${BOLD}${TEXT_RED}\3${RESET_FORMATTING}, Skipped: ${BOLD}${TEXT_YELLOW}\4${RESET_FORMATTING}/g"
+
+  # Make sure formatting is reset
+  echo -ne ${RESET_FORMATTING}
+  )
 }
 
 changed-mvn-projects() {
